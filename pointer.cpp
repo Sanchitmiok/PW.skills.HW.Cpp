e76e1a4352ebd3bd8c32ebca8c4a84b0d42c33d0
Sanchitmiok <116272108+Sanchitmiok@users.noreply.github.com>
2023-03-14 17:25:57 +0530

    pointerQ3
    Q3 - Write a function that returns the largest and the smallest number out of 3 given numbers.
    The main function should call this function to get the largest and smallest number and print
    the output.

---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+// Write a program to find the product of 2 numbers using pointers
+#include<iostream>
+using namespace std;
+int maxandmin(int a,int b ,int c,int *smallest){
+*smallest=min(a,min(b,c));
+return max(a,max(b,c));
+}
+int main(){
+int a;int b;int c;
+cin>>a>>b>>c;
+int smallest;
+int max=maxandmin(a,b,c,&smallest);
+cout<<max<<" and "<<smallest;
+    return 0 ;
+}
+
