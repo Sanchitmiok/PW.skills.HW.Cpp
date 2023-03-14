bb4aef7639326cf7eef24bd8f9d78f699513ae5a
Sanchitmiok <116272108+Sanchitmiok@users.noreply.github.com>
2023-03-14 17:43:30 +0530

    pointetQ4
    Q4 - Implement the swap function using pointers.

--- pointer.cpp
+++ pointer.cpp
@@ -1,16 +1,21 @@
-//pointer
+
 #include<iostream>
 using namespace std;
-int maxandmin(int a,int b ,int c,int *smallest){
-*smallest=min(a,min(b,c));
-return max(a,max(b,c));
+int swap(int *ada,int*adb){
+     int temp=*ada;
+     *ada=*adb;
+     *adb=temp;
+    
 }
 int main(){
-int a;int b;int c;
-cin>>a>>b>>c;
-int smallest;
-int max=maxandmin(a,b,c,&smallest);
-cout<<max<<" and "<<smallest;
+int a,b;
+   cin>>a>>b;
+   cout<<"a="<<a<<"  b="<<b<<endl;
+int *ada=&a;
+int *adb=&b;
+   swap(&a,&b);
+   cout<<"a="<<a<<"  b="<<b<<endl;
+
     return 0 ;
 }
 
