6558fcb1841ebbd9a4f762b40ecdb99fea0f0b73
Sanchitmiok <116272108+Sanchitmiok@users.noreply.github.com>
2023-03-15 14:10:05 +0530

    recurtion problem
    write a function to find out p^q using the concept of recurtion

---
+++ Recurtion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fun(int p ,int  q){
+    if(q==1) return p;
+    return (fun(p,q-1) *p);
+    
+
+}
+int main(){
+
+int p,q;
+cout<<"enter p and q :"<<endl;
+cin>>p>>q;
+cout<<p <<" and "<<q<<endl;
+int result =fun(p,q);
+cout<<"the p^q is : "<<result;
+    return 0;
+}
