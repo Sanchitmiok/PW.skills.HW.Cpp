8b8524cca503fd34ab1d363b2409d3167ce23f23
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2023-07-12 15:46:31 +0530

    Create Reverse the list.cpp

---
+++ Linkedlist/problem/Reverse the list.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+     int value;
+     Node *Add;
+};
+class LL
+{
+public:
+     Node *head;
+
+public:
+     LL()
+     {
+          head = nullptr;
+     }
+     void Add_tail(int val)
+     {
+          Node *newnode = new Node;
+          newnode->value = val;
+          newnode->Add = nullptr;
+          if (head == nullptr)
+          {
+               head = newnode;
+          }
+          else
+          {
+               Node *current = head;
+               while (current->Add != nullptr)
+               {
+                    current = current->Add;
+               }
+               current->Add = newnode;
+          }
+     }
+     void Display()
+     {
+          Node *current = head;
+          while (current != nullptr)
+          {
+               cout << current->value << "->";
+               current = current->Add;
+          }
+          cout << "NULL"<<endl;
+     }
+     
+};
+Node*Reverse_list(Node* &head){
+     Node*current = head;
+     Node*Prev = nullptr;
+     while(current){
+          Node*next=current->Add;
+          current->Add=Prev;
+          Prev=current;
+          current=next;
+     }
+     return Prev;
+}
+
+
+int main()
+{
+     LL ll;
+     ll.Add_tail(1);
+     ll.Add_tail(2);
+     ll.Add_tail(3);
+     ll.Add_tail(4);
+     ll.Add_tail(5);
+     ll.Add_tail(6);
+     ll.Display();  
+     ll.head= Reverse_list(ll.head);
+     ll.Display();
+     return 0;
+}
