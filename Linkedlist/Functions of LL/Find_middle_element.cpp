6fffb2c47b71fad0b63ada2053c09c6757f17d53
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2023-07-14 01:15:35 +0530

    Create Find_middle_element.cpp

---
+++ Linkedlist/Functions of LL/Find_middle_element.cpp
@@ -0,0 +1,12 @@
+Node* findMiddleElement() {
+    if (head == NULL) {
+        return NULL;
+    }
+    Node* slow = head;
+    Node* fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
