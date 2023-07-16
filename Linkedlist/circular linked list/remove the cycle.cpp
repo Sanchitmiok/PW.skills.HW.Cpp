3b877ced825ab1acf264c583074d0c5fcfab143d
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2023-07-17 01:22:08 +0530

    Create remove the cycle.cpp

---
+++ Linkedlist/circular linked list/remove the cycle.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+class NODE
+{
+public:
+     int value;
+     NODE *next;
+};
+class Linkedlist
+{
+public:
+     NODE *head;
+
+public:
+     Linkedlist()
+     {
+          head = NULL;
+     }
+     void insertAthead(int value)
+     {
+          NODE *newnode = new NODE;
+          newnode->value = value;
+          newnode->next = head;
+          head = newnode;
+     }
+     void display()
+     {
+          NODE *Next = head;
+          while (Next != NULL)
+          {
+               cout << Next->value << "->";
+               Next = Next->next;
+          }
+          cout << "NULL";
+     }
+};
+NODE *Cycle_inLL(NODE *head)
+{
+     if (!head || !head->next)
+     {
+          return nullptr;
+     }
+     NODE *slow = head;
+     NODE *fast = head;
+     bool hascycle = false;
+     while (fast && fast->next)
+     {
+          slow = slow->next;
+          fast = fast->next->next;
+          if (slow == fast)
+          {
+               hascycle = true;
+               break;
+          }
+
+     } // till here we have found either cycle is present or not
+     if (hascycle)
+     { // to found the starting point of cycle
+          slow = head;
+          while (slow != fast)
+          {
+               slow = slow->next;
+               fast = fast->next;
+          }
+          return slow;
+     }
+     else
+     {
+          return nullptr;
+     }
+}
+void removethecycle(NODE *head) // to remove the cycle from list
+{
+     if (!head || !head->next)
+     {
+          return;
+     }
+     NODE *slow = head;
+     NODE *fast = head;
+     bool flag = false;
+     while (fast && fast->next)
+     {
+          slow = slow->next;
+          fast = fast->next->next;
+          if (fast == slow)
+          {
+               flag = true;
+               break;
+          }
+     }
+     if (flag)
+     {
+          slow = head;
+          while (slow->next != fast->next)
+          {
+               slow = slow->next;
+               fast = fast->next;
+          }
+          fast->next = nullptr;
+     }
+}
+int main()
+{
+     Linkedlist ll;
+     ll.insertAthead(2);
+     ll.insertAthead(1);
+     ll.insertAthead(5);
+     ll.insertAthead(9);
+     ll.head->next->next->next = ll.head->next->next;
+     removethecycle(ll.head);
+     NODE *Founded_node = Cycle_inLL(ll.head);
+     if (Founded_node)
+     {
+          cout << "Cycle is present at " << Founded_node->value;
+     }
+     else
+          cout << "No cycle is present";
+     return 0;
+}
