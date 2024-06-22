d8e9dd887fa547b71cce2989c2a3304c763370d9
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2024-06-22 20:07:16 +0530

    Create radixSort.cpp

---
+++ Sorting/radixSort.cpp
@@ -0,0 +1,24 @@
+void countingSortForRadix(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n) {
+    int maxVal = *std::max_element(arr, arr +
