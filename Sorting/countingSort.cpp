3f57803dbc3e999611d31d0b9f0155254363a667
Sanchit Mishra <116272108+Sanchitmiok@users.noreply.github.com>
2024-06-22 20:06:22 +0530

    Create countingSort.cpp

---
+++ Sorting/countingSort.cpp
@@ -0,0 +1,21 @@
+void countingSort(int arr[], int n, int maxVal) {
+    int count[maxVal + 1] = {0};
+    int output[n];
+
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= maxVal; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
